db4fbe208ddbf81f4929de25c8d0a15fae6dd3b1
Hauagge <gabriel.dev@agroflux.ind.br>
2023-04-13 17:23:50 -0300

    create orgs use case

--- src/useCase/Org/CreateOrgsUseCase.ts
+++ src/useCase/Org/CreateOrgsUseCase.ts
@@ -1,16 +1,37 @@
 import { Prisma } from '@prisma/client';
+import { hash } from 'bcryptjs';
 import { EmailAlreadyRegisteredError } from '../../errors/EmailAlreadyExists';
 import { OrgsRepository } from '../../repository/OrgsRepositort';
 
 class CreateOrgsUseCase {
     constructor(private orgsRepository: OrgsRepository) {}
 
-    async execute(data: Prisma.OrgsCreateInput) {
-        const orgAlreadyExists = await this.orgsRepository.findByEmail(
-            data.email
-        );
+    async execute({
+        organizationName,
+        ownerName,
+        email,
+        password,
+        phone,
+        address,
+        city,
+        latitude,
+        longitude,
+        zipCode,
+    }: Prisma.OrgsCreateInput) {
+        const orgAlreadyExists = await this.orgsRepository.findByEmail(email);
         if (orgAlreadyExists) throw new EmailAlreadyRegisteredError();
-        const org = await this.orgsRepository.create(data);
+        const org = await this.orgsRepository.create({
+            organizationName,
+            ownerName,
+            email,
+            password: await hash(password, 6),
+            phone,
+            address,
+            city,
+            latitude,
+            longitude,
+            zipCode,
+        });
         return { org };
     }
 }
