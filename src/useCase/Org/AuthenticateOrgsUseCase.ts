5689acb6b78ff048c20c5bdbef0fdb33ada949f9
Hauagge <gabriel.dev@agroflux.ind.br>
2023-04-13 17:23:38 -0300

    authentication usecase

---
+++ src/useCase/Org/AuthenticateOrgsUseCase.ts
@@ -0,0 +1,28 @@
+import { compare } from 'bcryptjs';
+import { InvalidCredentialsError } from '@/errors/InvalidCredentials';
+import { IOrgsRepository } from '@/repository/IOrgsRepository';
+
+interface IAuthenticateUseCase {
+    email: string;
+    password: string;
+}
+
+export class AuthenticateUseCase {
+    constructor(private orgRepository: IOrgsRepository) {}
+
+    async execute({ email, password }: IAuthenticateUseCase) {
+        const org = await this.orgRepository.findByEmail(email);
+
+        if (!org) {
+            throw new InvalidCredentialsError();
+        }
+
+        const doesPasswordMatches = await compare(password, org.password);
+
+        if (!doesPasswordMatches) {
+            throw new InvalidCredentialsError();
+        }
+
+        return { org };
+    }
+}
