5f9f99ac0c4395d2faae35cb2a4034389adc4aa9
Hauagge <gabriel.dev@agroflux.ind.br>
2024-01-18 13:21:41 -0300

    Remove delete and update methods from IRequirementsRepository

--- src/repository/RequirementsRepositories/IRequirementsRepository.ts
+++ src/repository/RequirementsRepositories/IRequirementsRepository.ts
@@ -5,8 +5,6 @@ interface IRequirementsRepository {
     create(requirement: Requirements): Promise<Requirements>;
     list(): Promise<Requirements[]>;
     findById(id: string): Promise<Requirements | undefined>;
-    delete(id: string): Promise<void>;
-    update(requirement: Requirements): Promise<void>;
     findByDescription(description: string): Promise<Requirements | undefined>;
 }
 
