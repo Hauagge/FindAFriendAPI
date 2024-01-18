87cc3c6aca18a52f15215f30111bf1d353bfe9ca
Hauagge <gabriel.dev@agroflux.ind.br>
2024-01-18 13:21:57 -0300

    Add RequirementsRepositoryInMemory implementation

---
+++ src/repository/RequirementsRepositories/RequirementsRepositoryInMemery.ts
@@ -0,0 +1,32 @@
+import { Requirements } from "@/models/Requeriments";
+import { IRequirementsRepository } from "./IRequirementsRepository";
+
+
+
+class RequirementsRepositoryInMemory implements IRequirementsRepository {
+  async list(): Promise<Requirements[]> {
+     
+    return this.requirements;
+  }
+ async findById(id: string): Promise<Requirements | undefined> {
+     const requirement = this.requirements.find((requirement) => requirement.id === id);
+        return requirement;
+  }
+ 
+  private requirements: any[] = [];
+
+  async create({ description }: any): Promise<any> {
+    const requirement = new Object();
+    Object.assign(requirement, {
+      description,
+      id: Math.random().toString(),
+    });
+    this.requirements.push(requirement);
+    return requirement;
+  }
+  async findByDescription(description: string): Promise<any> {
+    return this.requirements.find((requirement) => requirement.description === description);
+  }
+}
+
+export { RequirementsRepositoryInMemory };
