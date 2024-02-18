1e26e604ea356307ee32c65e38c3dfb81c5ae783
Hauagge <gabriel.dev@agroflux.ind.br>
2024-02-18 19:56:38 -0300

    Add CreatePetController test and handle optional files in CreatePetController

---
+++ src/useCase/Pets/CreatePet/CreatePetController.test.ts
@@ -0,0 +1,56 @@
+import request from 'supertest';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+import { app } from '../../../app';
+
+
+describe('Create Pet Controller', () => {
+
+
+    beforeAll(async () => {
+        await app.ready();
+    });
+
+    afterAll(async () => {
+        await app.close();
+    });
+
+
+    it('should be able create a new pet', async () => {
+        await request(app.server).post('/api/v1/orgs').send({
+            organizationName:'PAES',
+            ownerName:'Jhon Doe',
+            email:'email@test.com',
+            password:'123456',
+            phone:'1234565678',
+            address:'Rua das FLores',
+            city:'São Paulo',
+            state:'SP',
+            latitude:'123456',
+            longitude:'123456',
+            zipCode:'123456',
+        });
+
+        const authenticate = await request(app.server).post('/api/v1/orgs/authenticate').send({
+            email:'email@test.com',
+            password:'123456',
+        });
+
+        const response =  await request(app.server).post('/api/v1/pets')
+        .set('Authorization', `Bearer ${authenticate.body.token}`)
+        .send({
+            about: 'some description',
+            age: 'toddler', 
+            energyLevel:' highest'  ,
+            environment: 'production',
+            independencyLevel: 'production', //
+            name:'some name here',
+            size: 'big',
+            requirements: ['free space', 'food', 'water'],
+        });
+       
+        expect(response.status).toBe(201);
+        expect(response.body).toHaveProperty('id');
+    });
+  
+}
+);
