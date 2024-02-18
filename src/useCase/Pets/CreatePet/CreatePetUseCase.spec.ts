ecfd2e832d7f03b2dee6a68f79f0ad85720410d3
Hauagge <gabriel.dev@agroflux.ind.br>
2024-02-18 19:56:48 -0300

    Refactor CreatePetUseCase.spec.ts and add new properties to the CreatePetsUseCase class

--- src/useCase/Pets/CreatePet/CreatePetUseCase.spec.ts
+++ src/useCase/Pets/CreatePet/CreatePetUseCase.spec.ts
@@ -1,38 +1,40 @@
 
-import { HashProviderInMemory } from '@/providers/hashProvider/HashProviderInMemory';
+import { RequirementsRepositoryInMemory} from '@/repository/RequirementsRepositories/RequirementsRepositoryInMemory';
 import { PetsRepositoryInMemory } from '@/repository/PetsRepositories/PetsRepositoryInMemory';
 import { beforeEach, describe, expect, it } from 'vitest';
 import { CreatePetsUseCase } from './CreatePetUseCase';
+import { PetsRequirementsRepositoryInMemory } from '@/repository/PetsRequirementsRepository/PetsRequirementsRepositoryInMemory';
+import { ImageRepositoryInMemory } from '@/repository/ImageRepository/ImagesRepositoryInMemory';
 
 let sut:CreatePetsUseCase;
 let petsRepositoryInMemory: PetsRepositoryInMemory;
 let requirementsRepositoryInMemory: RequirementsRepositoryInMemory;
-let petRequirementsRepositoryInMemory: PetRequirementsRepositoryInMemory;
-let imagePetRepositoryInMemory: ImagePetRepositoryInMemory;
+let petRequirementsRepositoryInMemory: PetsRequirementsRepositoryInMemory;
+let imagePetRepositoryInMemory: ImageRepositoryInMemory;
 
-let hashProviderInMemory: HashProviderInMemory;
 describe('Create Pet UseCase', () => {
     beforeEach(async()=>{
 
         petsRepositoryInMemory = new PetsRepositoryInMemory();
-        hashProviderInMemory = new HashProviderInMemory();
-        sut = new CreatePetsUseCase(petsRepositoryInMemory,);
+        requirementsRepositoryInMemory = new RequirementsRepositoryInMemory();
+        petRequirementsRepositoryInMemory = new PetsRequirementsRepositoryInMemory();
+        imagePetRepositoryInMemory = new ImageRepositoryInMemory();
+        sut = new CreatePetsUseCase(petsRepositoryInMemory,requirementsRepositoryInMemory,petRequirementsRepositoryInMemory,imagePetRepositoryInMemory);
 
     })
     it('should be able to create a new org',async ()=>{
-        const newOrg = await sut.execute({
-            organizationName: 'test',
-            ownerName: 'test',
-            email: 'email@test.com',
-            password: '1234',
-            phone: '1234',
-            address: '1234',
-            city: '1234',
-            latitude: '1234',
-            longitude: '1234',
-            zipCode: '1234',
+        const newPet = await sut.execute({
+       about: 'some description',
+       age: 'toddler', 
+       energyLevel:' highest'  ,
+       environment: 'production',
+       independencyLevel: 'production', //
+       name:'some name here',
+       orgId: 1,
+       size: 'big',
+       requirements: ['free space', 'food', 'water'],
+       
         })
-
-        expect(newOrg.org).toHaveProperty('id');
+        expect(newPet).toHaveProperty('id');
     })
 })
